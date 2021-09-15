46b2ce536a209021b2df569c1e1f6e7ec018e3e4
WMF00 <wfriedrich593@g.rwu.edu>
2021-09-15 13:48:05 -0400

    Assignment 2

--- main.py
+++ main.py
@@ -3,14 +3,41 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def my_function():
+    #get user's name
+    name = input("Tell me your name:")
+    #print user's name
+    print(f'Hello{name}')
 
-def print_hi(name):
+def second_function():
+    #ask the user for a number
+    number = int(input("input a number:"))
+    #multiply number by itself
+    your_Number = number * number
+    #print number
+    print (your_Number)
+
+def third_function():
+    #ask user for a word
+    word = input("input a word:")
+    #count how many letters in the word
+    letters = len(word)
+    #print letters in word
+    print(letters)
+
+
+#def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    #print_hi('PyCharm')
+    my_function()
+    second_function()
+    third_function()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
