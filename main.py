17eea6b7a4218ed97ee6cee8ba8fde1130c19f6b
WMF00 <wfriedrich593@g.rwu.edu>
2021-10-24 13:50:15 -0400

    here I implimented the randint feature

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+import datetime
+from random import randint
+import calendar
+
+def feature1():
+    x = datetime.datetime.now()
+    print(x)
+
+
+def feature2():
+    for i in range(10):
+        print(randint(1, 25))
+
+def feature3():
+    yy = 2000
+    mm = 6
+    print(calendar.month(yy, mm))
 
 
 
-def twoD():
-    array = [['.', '.', '.', '.', '.', '.'],
-        ['|', '-', '-', '-', '-', '|'],
-        ['|', '|', '-', '-', '|', '|'],
-        ['|', '|', '|', '|', '|', '|'],
-        ['|', '|', '-', '-', '|', '|'],
-        ['|', '-', '-', '-', '-', '|'],
-        ['.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.']]
-    print("array")
-    for i in array:
-        for j in i:
-            print(j, end=" ")
-        print()
+#def twoD():
+    #array = [['.', '.', '.', '.', '.', '.'],
+        #['|', '-', '-', '-', '-', '|'],
+        #['|', '|', '-', '-', '|', '|'],
+        #['|', '|', '|', '|', '|', '|'],
+        #['|', '|', '-', '-', '|', '|'],
+        #['|', '-', '-', '-', '-', '|'],
+        #['.', '.', '.', '.', '.', '.'],
+        #['.', '.', '.', '.', '.', '.'],
+        #['.', '.', '.', '.', '.', '.']]
+    #print("array")
+    #for i in array:
+        #for j in i:
+            #print(j, end=" ")
+        #print()
 
 
-import random
-def coinFlip():
-    number = int(input("Heads'(0)' Tails'(1)':"))
-    flip = random.randint(0, 1)
-    if flip == 0:
-        print("Heads")
-    else:
-        print("Tails")
-    if number == flip:
-        print("You Win!")
-    else:
-        print("You Lost")
+#import random
+#def coinFlip():
+    #number = int(input("Heads'(0)' Tails'(1)':"))
+    #flip = random.randint(0, 1)
+    #if flip == 0:
+        #print("Heads")
+    #else:
+        #print("Tails")
+    #if number == flip:
+        #print("You Win!")
+    #else:
+        #print("You Lost")
 
 
 #def my_function():
@@ -72,9 +85,11 @@ if __name__ == '__main__':
     #my_function()
     #second_function()
     #third_function()
-    coinFlip()
-    twoD()
-
+    #coinFlip()
+    #twoD()
+    feature1()
+    feature2()
+    feature3()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
